20b95f8d42fff902eec2041e5685cbd9ad2c7549
JHKIMS <wjdgns5131@gmail.com>
2022-12-01 09:52:16 +0900

    Vue_Watcher로 데이터 감시(백스페이스도 문자 취급 -수정필요)

--- src/Modal.vue
+++ src/Modal.vue
@@ -5,6 +5,7 @@
             <p>{{ onerooms[userClick].content }} <br></p>
             <!-- <input @input="month = $event.target.value"> -->
             <input v-model.number="month">
+
             <p>{{month}}개월 선택함:{{ onerooms[userClick].price }}만원 <br></p>
             <button @click="modalClose">닫기</button>
         </div>
@@ -16,6 +17,14 @@ export default {
     data(){
         return{
             month : 1,
+        }
+    },
+    watch : {
+        month(a){ //a: 변경후 데이터, b: 변경전 데이터
+            this.month = 1;
+            if(!isNaN(a)) alert('문자 불가능')
+            // console.log(b);
+            // if(a>=13) alert('13이상 입력X')
         }
     },
     props : {
