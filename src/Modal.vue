05b2e4375aaa38e87b0af60c39459400aa2445e8
JHKIMS <wjdgns5131@gmail.com>
2022-12-01 09:16:59 +0900

    Vue_v-model사용(input)

--- src/Modal.vue
+++ src/Modal.vue
@@ -2,8 +2,10 @@
     <div v-if="modal == true" class="black-bg">
         <div class="white-bg">
             <h4>{{ onerooms[userClick].title }}</h4>
-            {{ onerooms[userClick].content }} <br>
-            {{ onerooms[userClick].price }}만원 <br>
+            <p>{{ onerooms[userClick].content }} <br></p>
+            <!-- <input @input="month = $event.target.value"> -->
+            <input v-model.number="month">
+            <p>{{month}}개월 선택함:{{ onerooms[userClick].price }}만원 <br></p>
             <button @click="modalClose">닫기</button>
         </div>
     </div>
@@ -11,6 +13,11 @@
 <script>
 export default {
     name: 'ModalVue',
+    data(){
+        return{
+            month : 1,
+        }
+    },
     props : {
         onerooms : Array,
         userClick : Number,
